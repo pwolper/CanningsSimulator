e18fa082ca0f716528c6f387d361ed07d99c8c03
Philip L. Wolper <phi.wolper@tum.de>
2024-03-13 18:33:04 +0100

    added simulation caller script for poisson

---
+++ scripts/fixation_probability_poisson.sh
@@ -0,0 +1,3 @@
+julia simulator.jl -N 1000 -t viability_constant -s 0.1 -m Poisson -p 1  --type probability -c constant -o fixation_probability --nb_simulations 500000
+julia simulator.jl -N 1000 -t viability_constant -s 0.01 -m Poisson -p 1  --type probability -c constant -o fixation_probability --nb_simulations 500000
+julia simulator.jl -N 1000 -t viability_constant -s 0.0 -m Poisson -p 1  --type probability -c constant -o fixation_probability --nb_simulations 500000
