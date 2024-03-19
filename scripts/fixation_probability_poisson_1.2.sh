13ea7c42c55a1f5162ca07c35cfaf3a282a18a7d
Philip L. Wolper <phi.wolper@tum.de>
2024-03-19 09:03:55 +0100

    modified poisson scripts

--- scripts/fixation_probability_poisson_1.2.sh
+++ scripts/fixation_probability_poisson_1.2.sh
@@ -1,13 +1,11 @@
-julia simulator.jl -N 1000 -s 0 -i 1 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s 0.001 -i 1 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s 0.005 -i 1 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s 0.01 -i 1 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s 0.05 -i 1 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s 0.1 -i 1 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-
-julia simulator.jl -N 1000 -s 0 -i 999 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s -0.001 -i 999 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s -0.005 -i 999 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s -0.01 -i 999 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s -0.05 -i 999 -m Poisson -p 1.2 -o sim --nb_simulations 500000
-julia simulator.jl -N 1000 -s -0.1 -i 999 -m Poisson -p 1.2 -o sim --nb_simulations 500000
+julia simulator.jl -N 1000 -s 0 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s 0.001 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s 0.005 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s 0.01 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s 0.05 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s 0.1 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s -0.001 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s -0.005 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s -0.01 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s -0.05 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
+julia simulator.jl -N 1000 -s -0.1 -I 0.5 -m Poisson -p 1.2 -o cluster --nb_simulations 500000
